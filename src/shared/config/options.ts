fc3054799d99a3048e6e6cd286b3492dc3275ef5
Tiago Vila Verde <tiago.vilaverde@elastic.co>
2026-06-15 18:28:01 +0100

    remove z3 and old v1 solver

--- src/shared/config/options.ts
+++ src/shared/config/options.ts
@@ -1,14 +1,5 @@
-import { Context } from "z3-solver";
-
 export const options = {
 	verbose: false,
 	showJS: false,
 	showElaboration: false,
 };
-
-let Z3: Context<"main"> | undefined = undefined;
-
-export const setZ3Context = (ctx: Context<"main">) => {
-	Z3 = ctx;
-};
-export const getZ3Context = () => Z3;
--- src/verification/modalities/liquids.ts
+++ src/verification/modalities/liquids.ts
@@ -1,15 +1,7 @@
 import * as EB from "@yap/elaboration";
 import * as NF from "@yap/elaboration/normalization";
 
-import * as Q from "@yap/shared/modalities/multiplicity";
 import * as Lit from "@yap/shared/literals";
-import * as Sub from "@yap/elaboration/unification/substitution";
-
-import * as Modal from "@yap/verification/modalities/shared";
-import { Context } from "z3-solver";
-import { defaultContext } from "@yap/shared/lib/primitives";
-
-import * as Z3 from "z3-solver";
 
 const tru = () => NF.Constructors.Lit({ type: "Bool", value: true });
 const fls = () => NF.Constructors.Lit({ type: "Bool", value: false });
@@ -26,7 +18,6 @@ type Predicate = {
 	Kind: (ctx: EB.Context, arg: NF.Value) => NF.Value;
 	Neutral: (ann: EB.Term) => EB.Term;
 	NeutralNF: (ann: NF.Value, ctx: EB.Context) => NF.Value;
-	True: (Z3: Context<"main">) => Z3.Bool;
 };
 
 export const Predicate: Predicate = {
@@ -39,6 +30,4 @@ export const Predicate: Predicate = {
 		const closure = NF.Constructors.Closure(ctx, EB.Constructors.Lit(Lit.Bool(true)));
 		return NF.Constructors.Lambda(fresh(), "Explicit", closure, ann);
 	},
-
-	True: (Z3: Context<"main">) => Z3.Bool.val(true),
 };
